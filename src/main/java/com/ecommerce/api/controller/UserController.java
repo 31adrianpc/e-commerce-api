6f41d892f69a8474c4775e711195e14257875e92
apc <apicoyc@gmail.com>
2025-12-22 21:53:29 -0500

    refactor: reemplazar @Autowired por inyección por constructor con @RequiredArgsConstructor

--- src/main/java/com/ecommerce/api/controller/UserController.java
+++ src/main/java/com/ecommerce/api/controller/UserController.java
@@ -1,21 +1,20 @@
 package com.ecommerce.api.controller;
 
+import com.ecommerce.api.dto.request.*;
 import com.ecommerce.api.dto.response.UserResponseDTO;
 import com.ecommerce.api.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping
     public ResponseEntity<List<UserResponseDTO>> listAll() {
