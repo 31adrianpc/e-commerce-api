95bfad667f4bfe8de9a4640845a3c8c1fc64826a
apc <apicoyc@gmail.com>
2025-12-24 11:04:55 -0500

    feat: implementación inicial de JwtService

---
+++ src/main/java/com/ecommerce/api/service/JwtService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.api.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String generateToken(Authentication authentication){
+        return null;
+    }
+}
