c04bfdb81b45e05ec7c510a84cfa6d34f7b15564
apc <apicoyc@gmail.com>
2026-01-17 18:30:56 -0500

    feat: añadir @SuperBuilder y @NoArgsConstructor a UserRegisterRequestDTO

--- src/main/java/com/ecommerce/api/dto/request/UserRegisterRequestDTO.java
+++ src/main/java/com/ecommerce/api/dto/request/UserRegisterRequestDTO.java
@@ -1,9 +1,12 @@
 package com.ecommerce.api.dto.request;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@NoArgsConstructor
 public class UserRegisterRequestDTO extends UserBaseDTO{
 
 }
